eb6545e4f5d94e8b2c8d15afada50059bc8fa758
DevtectiveFlex <148262074+DevtectiveFlex@users.noreply.github.com>
2025-01-06 03:01:15 +0300

    feat: add implementation for task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+const turnsToWin = 2 ** disksNumber - 1;
+const time = Math.floor((turnsToWin / turnsSpeed).toFixed(5) * 3600);
+const result = {
+  turns: turnsToWin,
+  seconds: time,
 }
-
+return result;
+}
 module.exports = {
   calculateHanoi
 };
